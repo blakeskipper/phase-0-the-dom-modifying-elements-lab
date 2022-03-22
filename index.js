f9bf9d0af10c184565b8dcff583eaabd6a29656b
blakeskipper <blakeskipper@gmail.com>
2022-03-22 19:38:38 -0400

    commit tree 3b8302d838e5f82acd1eec67c2f1a70958900b1d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = '<h1>victory</h1><p>BLAKE SKIPPER is the champion</p>'
+document.body.appendChild(newHeader);
